1f1eb9e19cabd935fb2c9996e37a08da06f0794a
Marcus Harrison <marcus@harrisonland.co.uk>
2021-12-26 20:24:22 +0100

    Add header stuff for rwlock

--- src/platforms/posix/csalt/platform/threads.h
+++ src/platforms/posix/csalt/platform/threads.h
@@ -16,6 +16,15 @@ typedef pthread_mutex_t csalt_mutex;
 #define csalt_mutex_unlock(mutex) pthread_mutex_unlock(mutex)
 #define csalt_mutex_deinit(mutex) pthread_mutex_destroy(mutex)
 
+typedef pthread_rwlock_t csalt_rwlock;
+
+#define csalt_rwlock_init(...) pthread_rwlock_init(__VA_ARGS__)
+#define csalt_wrlock_rdlock(rwlock) pthread_rwlock_rdlock(rwlock)
+#define csalt_rwlock_wrlock(rwlock) pthread_rwlock_wrlock(rwlock)
+#define csalt_rwlock_tryrdlock(rwlock) pthread_rwlock_tryrdlock(rwlock)
+#define csalt_rwlock_trywrlock(rwlock) pthread_rwlock_trywrlock(rwlock)
+#define csalt_rwlock_deinit(rwlock) pthread_rwlock_destroy(rwlock)
+
 #ifdef __cplusplus
 } // extern "C"
 #endif
